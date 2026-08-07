a5d10ca23f1f7bcad45f32ee720d15ba86513efa
kaiioneda <laura.fagotte@escola.pr.gov.br>
2026-08-06 22:42:15 -0300

    jhusedihfo

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorAll("button);
+
+    botoes.forEach(function (botoa) {
+        let curtiu = false;
+        botao.addEventListener("click", botaoClicado);
+        function botaoClicado () {
+            conslole.log("fui clicado");
+            let texto = botao.add
+        }
+    })
