463eee60a2ba9a3b217944f647a87755e664698b
jwendel-jpg <j.wendel@escola.pr.gov.br>
2026-08-10 22:14:01 -0300

    10/08

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-const botoes = document.querySelectorAll("button);
+const botoes = document.querySelectorAll("button");
 
     botoes.forEach(function (botoa) {
         let curtiu = false;
         botao.addEventListener("click", botaoClicado);
         function botaoClicado () {
             conslole.log("fui clicado");
-            let texto = botao.add
+            let texto = botao.querySelector("span");
+            if (curtiu === false) {
+                texto.textContend++;
+                curtiu = true;
+            } else {
+                texto.textContend--;
+                curtiu = false;
+            }
         }
     })
